e059fc6998f81d30b123a7a4b147b5f3d0701eec
Harsh Prakash <harshprakash1999@gmail.com>
2025-11-19 15:29:38 +0530

    Fix CORS error.

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,8 +4,10 @@ import express from "express";
 // Create express app
 const app = express();
 
+const CORS_ORIGIN = process.env.CORS_ORIGIN;
+
 // Middlewares
-app.use(cors());
+app.use(cors({ origin: CORS_ORIGIN, credentials: true }));
 app.use(express.json({ limit: "16kb" }));
 
 // Routes
