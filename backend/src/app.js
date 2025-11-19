0928262b8ef65398c9537e6f107fe5786375de30
Harsh Prakash <harshprakash1999@gmail.com>
2025-11-19 15:50:36 +0530

    FIxing CORS Error.

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 const CORS_ORIGIN = process.env.CORS_ORIGIN;
 
 // Middlewares
-app.use(cors({ origin: "*", credentials: true }));
+app.use(cors({ origin: CORS_ORIGIN, credentials: true }));
 app.use(express.json({ limit: "16kb" }));
 
 // Routes
